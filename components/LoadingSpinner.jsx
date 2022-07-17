e45735dc43ff2d28115040b026e2021d9a69e251
Hector Wong <hectorwong2110@gmail.com>
2022-07-16 19:57:43 -0700

    First commit of this app

---
+++ components/LoadingSpinner.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const LoadingSpinner = () => {
+  return (
+    <>
+    <div className='max-[200px]  m-auto block'>
+        CARGANDO..
+    </div>
+    </>
+  )
+}
+
+export default LoadingSpinner
