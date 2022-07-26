1c66a6ecab6913a2b7d2531b0f4e5d18be87044d
Hector Wong <hectorwong2110@gmail.com>
2022-07-26 10:17:33 -0700

    Cambiando a un fondo mejor

--- pages/index.js
+++ pages/index.js
@@ -36,7 +36,7 @@ export default function Home() {
 
         {/*Background Image*/}
         <Image
-          src={`https://images.unsplash.com/photo-1566667937341-0d579faa4597?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MjEyfHx3ZWF0aGVyfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=400&q=60`}
+          src={`https://images.unsplash.com/photo-1601134467661-3d775b999c8b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1075&q=80`}
           layout='fill'
           className='object-cover'
         />
